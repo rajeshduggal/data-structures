976a83261b380eb9834f7c80def8d765a9f29f81
Rajesh Duggal <rajesh@duggalmail.com>
2025-05-08 23:33:10 -0400

    Extract LinkedList code out of CycleInLinkedList class

--- CycleInLinkedList.java
+++ CycleInLinkedList.java
@@ -10,6 +10,8 @@ import picocli.CommandLine.Parameters;
 
 import static java.lang.System.*;
 
+//import LL;
+
 /**
  * Detect if there is a cycle in a linked list
  *
@@ -22,8 +24,6 @@ import static java.lang.System.*;
     version = "CycleInLinkedList 0.1",
     description = "Detect if there is a cycle in a linked list")
 class CycleInLinkedList implements Callable<Integer> {
-
-  private Node head = null;
 
   @Parameters(index = "0", arity = "0..*", description = "Initial set of values to load into the linked list")
   private List<String> inputStrings;
@@ -47,68 +47,9 @@ class CycleInLinkedList implements Callable<Integer> {
             "a b,b c,c d");
     }
     inputStrings.forEach( str -> {
-        Node head = buildLinkedList(str);
-        out.println(str + " : " + hasCycle(head));
+        LL<Character> myLinkedList = new LL<>(str);
+        out.println(str + " : " + myLinkedList.hasCycle());
     });
     return 0;
-  }
-
-  private Node buildLinkedList(String str) {
-    String[] pairs = str.split(",");
-    head = new Node((pairs[0].split(" ")[0]).charAt(0)); 
-
-    char[] arr;
-    for (String srcdest : pairs) {
-      arr = srcdest.toCharArray();
-      link(arr[0],arr[2]);
-    }
-    return head;
-  }
-
-  private boolean hasCycle(Node head) {
-      Node fast = head;
-      Node slow = head;
-
-      while (fast != null && fast.next != null) {
-          // out.println(" fast = " + fast.name);
-          // out.println(" slow = " + slow.name);
-          fast = fast.next.next;
-          slow = slow.next;
-          if (fast == slow) return true;
-      }
-      return false;
-  }
-
-  class Node {
-    Node next;
-    char name;
-
-    Node(char name) { this.name = name; }
-  }
-
-
-  private Node find(char term) {
-    Node current = head;
-    int counter = 0; // it might be a cycle, so we give up after 10 steps.
-    while (current != null && counter < 10) {
-        counter++;
-        if (current.name == term) return current;
-        current = current.next;
-    }
-    return null;
-  }
-
-  private void link(char src, char dest) {
-    //out.println("Linking " + src + " to " + dest);
-    Node destNode = find(dest);
-    if (destNode == null) {
-        destNode = new Node(dest);
-    }
-    Node srcNode = find(src);
-    if (srcNode == null) {
-        srcNode = new Node(src);
-        head = srcNode;
-    }
-    srcNode.next = destNode;
   }
 }
